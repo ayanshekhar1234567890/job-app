b992b5b3c3769e3b8d77412b2a1b8f5fa3db60f7
= <=>
2023-02-05 20:05:40 +0530

    SEtup part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { JobComponent } from './job/job.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    JobComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/job/job.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-job',
+  templateUrl: './job.component.html',
+  styleUrls: ['./job.component.scss']
+})
+export class JobComponent {
+
+}
