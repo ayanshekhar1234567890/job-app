b3bdb16d742eeb8adfd9138d1446676b8d833a96
= <=>
2023-05-05 13:30:04 +0530

    Daisy UI 🌼

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,7 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
-import { Design } from './app/job/Design';
 
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
-
-console.log(new Design("pc", "pc", "card").getPixelLength());
